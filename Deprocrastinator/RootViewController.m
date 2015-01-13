f35b4663fca9f89a0bd889d11cc77a3b8da44362
Aaron Bradley <aaronb@Aarons-MacBook-Air.local>
2015-01-12 22:50:21 -0600

    almost have the cancel alert working, but a few quirks

--- Deprocrastinator/RootViewController.m
+++ Deprocrastinator/RootViewController.m
@@ -8,15 +8,18 @@
 
 #import "RootViewController.h"
 
-@interface RootViewController () <UITableViewDelegate, UITableViewDataSource>
+@interface RootViewController () <UITableViewDelegate, UITableViewDataSource, UIAlertViewDelegate>
 
 @property (strong, nonatomic) IBOutlet UITableView *taskTableView;
-
 @property (strong, nonatomic) IBOutlet UITextField *taskTextField;
 
 @property NSString *userTask;
 @property NSMutableArray *userTasksArray;
 @property UITableViewCell *cell;
+
+//@property NSArray *priorityColorsArray;
+
+@property int numberOfSwipesToRight;
 
 //@property CGPoint taskTouched;
 
@@ -31,6 +34,10 @@
 
     self.userTasksArray = [[NSMutableArray alloc] init];
 
+//    self.priorityColorsArray = [[NSArray alloc] initWithObjects:[UIColor redColor], [UIColor yellowColor], [UIColor greenColor], nil];
+
+    self.numberOfSwipesToRight = 0;
+
 
 }
 
@@ -38,15 +45,49 @@
 ///////
 
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
 
-    forRowAtIndexPath:(NSIndexPath *)indexPath{
-    [self.userTasksArray removeObjectAtIndex:indexPath.row];
-    [self.taskTableView reloadData];
+    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Hold Up!" message:@"Are you sure you want to delete this item.  This can't be undone" delegate:self cancelButtonTitle:@"Delete" otherButtonTitles:@"Cancel", nil];
+    [alert show];
+
+//    [alert reloadInputViews];
+//    if (editingStyle == UITableViewCellEditingStyleDelete) {
+//        [self.userTasksArray removeObjectAtIndex:indexPath.row];
+//
+//    } else {
+//        [self alertView:alert clickedButtonAtIndex:1];
+//    }
+
     
 }
 
+- (void)alertViewCancel:(UIAlertView *)alertView {
+    [self.taskTableView reloadData];
+}
 
+- (void)alertView:(UIAlertView *)alertView didDismissWithButtonIndex:(NSInteger)buttonIndex {
+
+
+//            [self.userTasksArray removeObjectAtIndex:];
+            [self.taskTableView reloadData];
+}
+
+
+//- (void)alertView:(UIAlertView *)alertView clickedButtonAtIndex:(NSInteger)buttonIndex indexPath:(NSIndexPath *)indexPath{
+//
+//    if (buttonIndex == 0){
+//
+//        [self.userTasksArray removeObjectAtIndex:indexPath.row];
+//        [self.taskTableView reloadData];
+//
+//    } else {
+//
+//        [self.taskTableView reloadData];
+//
+//    }
+//
+//
+//}
 
 
 -(BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
@@ -58,10 +99,11 @@
 }
 
 - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)sourceIndexPath toIndexPath:(NSIndexPath *)destinationIndexPath {
+
     NSString *titleItem = [self.userTasksArray objectAtIndex:sourceIndexPath.row];
+
     [self.userTasksArray removeObject:titleItem];
     [self.userTasksArray insertObject:titleItem atIndex:destinationIndexPath.row];
-
 
     [self.taskTableView reloadData];
 
@@ -99,13 +141,36 @@
 }
 
 
+- (IBAction)onSwipeToRight:(UISwipeGestureRecognizer *)sender {
+
+    CGPoint location = [sender locationInView:self.taskTableView];
+    NSIndexPath *indexPath = [self.taskTableView indexPathForRowAtPoint:location];
+    UITableViewCell *swipedCell = [self.taskTableView cellForRowAtIndexPath:indexPath];
+
+
+    self.numberOfSwipesToRight++;
+
+    if (self.numberOfSwipesToRight == 1) {
+        swipedCell.textLabel.textColor = [UIColor redColor];
+    } else if (self.numberOfSwipesToRight == 2) {
+        swipedCell.textLabel.textColor = [UIColor yellowColor];
+    } else if (self.numberOfSwipesToRight == 3) {
+        swipedCell.textLabel.textColor = [UIColor greenColor];
+    } else {
+        swipedCell.textLabel.textColor = [UIColor blackColor];
+        self.numberOfSwipesToRight = 0;
+    }
+
+//    [self.taskTableView reloadData];
+
+}
+
 
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
 
-    UITableViewCell *cellGreen = [self.taskTableView cellForRowAtIndexPath:indexPath];
-
-    cellGreen.textLabel.textColor = [UIColor greenColor];
+    self.cell = [self.taskTableView cellForRowAtIndexPath:indexPath];
+    self.cell.textLabel.textColor = [UIColor greenColor];
 
 }
 
