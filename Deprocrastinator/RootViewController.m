f8b64527e5ff06efdc987efed604e8209c729718
Aaron Bradley <aaronb@Aarons-MacBook-Air.local>
2015-01-12 20:17:59 -0600

    completed green color change on tap

---
+++ Deprocrastinator/RootViewController.m
@@ -0,0 +1,92 @@
+//
+//  ViewController.m
+//  Deprocrastinator
+//
+//  Created by Aaron Bradley on 1/12/15.
+//  Copyright (c) 2015 Aaron Bradley. All rights reserved.
+//
+
+#import "RootViewController.h"
+
+@interface RootViewController () <UITableViewDelegate, UITableViewDataSource>
+
+@property (strong, nonatomic) IBOutlet UITableView *taskTableView;
+
+@property (strong, nonatomic) IBOutlet UITextField *taskTextField;
+
+@property NSString *userTask;
+@property NSMutableArray *userTasksArray;
+@property UITableViewCell *cell;
+
+//@property CGPoint taskTouched;
+
+@end
+
+
+
+@implementation RootViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+
+    self.userTasksArray = [[NSMutableArray alloc] init];
+
+
+}
+
+- (IBAction)onAddButtonPressed:(UIButton *)sender {
+
+    self.userTask = [NSString stringWithFormat:@"%@", self.taskTextField.text];
+
+    [self.userTasksArray addObject: self.taskTextField.text];
+    [self.taskTableView reloadData];
+
+    self.taskTextField.text = @"";
+    [self.taskTextField endEditing:YES];
+
+}
+- (IBAction)onEditButtonPressed:(UIBarButtonItem *)sender {
+
+    if (self.editing)
+    {
+        self.editing = false;
+        [self.taskTableView setEditing:false animated:true];
+        sender.style = UIBarButtonItemStylePlain;
+        sender.title = @"Edit";
+    } else {
+        self.editing = true;
+        [self.taskTableView setEditing:true animated:true];
+        sender.style = UIBarButtonItemStyleDone;
+        sender.title = @"Done";
+    }
+    [self.taskTableView reloadData];
+}
+
+
+
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    UITableViewCell *cellGreen = [self.taskTableView cellForRowAtIndexPath:indexPath];
+
+    cellGreen.textLabel.textColor = [UIColor greenColor];
+
+}
+
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.userTasksArray.count;
+}
+
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+    self.cell = [tableView dequeueReusableCellWithIdentifier:@"ColorCellID"];
+    self.cell.textLabel.text = [self.userTasksArray objectAtIndex:indexPath.row];
+
+    return self.cell;
+    
+}
+
+
+@end
